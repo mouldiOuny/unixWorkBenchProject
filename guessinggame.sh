420524fa30bde95d6d7ac010f0ea339f3a560d82
Mouldi Ouni <mouldi.ouni@sesame.com.tn>
2020-06-20 09:56:53 +0000

    write comments for the todo tasks

---
+++ guessinggame.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Find the number of files in the current directory 
+
+
+# while true 
+    # ask the user for the number of the files
+    # check that the  input is number
+    # if it is a correct answer print Congrats
+    # if it is lower print lower
+    # if it is higher print higher
